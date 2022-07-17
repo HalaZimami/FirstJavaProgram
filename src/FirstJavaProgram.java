3c669198718f682ae1b6d8546f3125a75616b3c3
Hala Alzimami <halazimami@gmail.com>
2022-07-17 11:08:55 +0300

    first commit

---
+++ src/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) throws Exception {
+        System.out.println("My name is Hala");
+        System.out.println("I'm 24 years old");
+        System.out.println("My hometown is Riyadh");
+    }
+}
